85ecec57818d5935e23f69f5515c7cc9c3c3bce7
Hana　Yoshimura <huacaijicun7@gmail.com>
2024-11-22 18:28:01 +0900

    Initial Commit

---
+++ SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by 吉村花菜 on 2024/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
