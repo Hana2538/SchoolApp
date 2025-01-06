a55240d1d41c58e3ddcde25a2223380b49660156
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-06 15:53:57 +0900

    アカウント作成するぞん

--- SchoolApp/LogInAccountView.swift
+++ SchoolApp/LogInAccountView.swift
@@ -68,7 +68,7 @@ struct LogInAccountView: View {
                 }
                 .padding(.horizontal, 20)
                 
-                // アカウントをお持ちでない方はこちらボタン
+                
                 NavigationLink(destination: SignUpView()) {
                     Text("アカウントをお持ちでない方はこちら")
                         .fontWeight(.bold)
@@ -86,33 +86,74 @@ struct LogInAccountView: View {
 }
 
 struct SignUpView: View {
+    @State private var Accountname: String = ""
+    @State private var Mailname: String = ""
+    @State private var Password: String = ""
+    
     var body: some View {
         ZStack {
             Image("kabegamiNone")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-        
             
             RoundedRectangle(cornerRadius: 25)
                 .fill(Color.orange)
                 .frame(width: 350, height: 500)
                 .opacity(0.5)
-                .padding(.top, -200)
             
             VStack {
+                Spacer().frame(height: 150)
                 
                 Text("アカウント作成")
                     .font(.system(size: 40))
                     .fontWeight(.bold)
-                    .padding(.top,50)
                     .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                    .padding(.bottom, 20) // 下の余白を少し広めに
+                
+                TextField("アカウント名", text: $Accountname)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    .padding(.horizontal)
+                    .frame(width: 350, height: 50)
+                    .padding(.bottom, 30)
+                
+                TextField("メールアドレス", text: $Mailname)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    .padding(.horizontal)
+                    .frame(width: 350, height: 50)
+                    .padding(.bottom, 30)
+                
+                TextField("パスワード", text: $Password)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(8)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                    .padding(.horizontal)
+                    .frame(width: 350, height: 50)
+                    .padding(.bottom, 30)
                 
                 Spacer()
             }
         }
     }
 }
+
+
 
 
 
