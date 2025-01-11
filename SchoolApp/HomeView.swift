93ac0b2540262062b971c816a84ca8d5bb9a83df
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-11 22:13:47 +0900

    Z軸に配置するのときボタンはどうなるんだろう？？

--- SchoolApp/HomeView.swift
+++ SchoolApp/HomeView.swift
@@ -4,67 +4,82 @@ struct HomeView: View {
     @State private var GamePin: String = ""
     var body: some View {
         ZStack {
+            
             Image("kabegamiNone")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
             
-            VStack {
-                TextField("ゲームPIN", text: $GamePin)
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(8)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
-                    .padding(.horizontal, 20)
-                    .frame(width: 400, height: 50)
-                    .padding(.top, -200)
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(height: 100)
                 
-                Button(action: {
-                    print("入力ボタンが押されました。")
-                }) {
-                    Text("入力")
-                        .fontWeight(.bold)
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 200, height: 60)
-                        .background(Color(red: 0, green: 0.4, blue: 0.7))
-                        .cornerRadius(8)
-                }
-                .padding(.top, -100)
+                Spacer()
                 
-                Button(action: {
-                    print("問題を新規作成ボタンが押されました。")
-                }) {
-                    Text("問題を新規作成")
-                        .fontWeight(.bold)
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
+                VStack {
+                    TextField("ゲームPIN", text: $GamePin)
                         .padding()
-                        .frame(width: 200, height: 60)
-                        .background(Color(.orange))
+                        .background(Color(.white))
                         .cornerRadius(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                        .padding(.horizontal, 20)
+                        .frame(width: 400, height: 50)
+                        .padding(.top, -200)
+                    
+                    Button(action: {
+                        print("入力ボタンが押されました。")
+                    }) {
+                        Text("入力")
+                            .fontWeight(.bold)
+                            .font(.system(size: 25))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 200, height: 60)
+                            .background(Color(red: 0, green: 0.4, blue: 0.7))
+                            .cornerRadius(8)
+                    }
+                    .padding(.top, -100)
+                    
+                    Button(action: {
+                        print("問題を新規作成ボタンが押されました。")
+                    }) {
+                        Text("問題を新規作成")
+                            .fontWeight(.bold)
+                            .font(.system(size: 25))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 200, height: 60)
+                            .background(Color(.orange))
+                            .cornerRadius(8)
+                    }
+                    .padding(.top)
+                    
+                    Button(action: {
+                        print("マイルームボタンが押されました。")
+                    }) {
+                        Text("マイルーム")
+                            .fontWeight(.bold)
+                            .font(.system(size: 25))
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 200, height: 60)
+                            .background(Color(.orange))
+                            .cornerRadius(8)
+                    }
+                    .padding(.top)
                 }
-                .padding(.top)
                 
-                Button(action: {
-                    print("マイルームボタンが押されました。")
-                }) {
-                    Text("マイルーム")
-                        .fontWeight(.bold)
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 200, height: 60)
-                        .background(Color(.orange))
-                        .cornerRadius(8)
-                }
-                .padding(.top)
+                Spacer()
+                
+                
+                Rectangle()
+                    .fill(Color(red: 0, green: 0.4, blue: 0.7))
+                    .frame(height: 100)
             }
-            .padding(.top, 200) // top,horizontalとかの意味がわかんない。どうしてここに書くんだろう？？
         }
     }
 }
