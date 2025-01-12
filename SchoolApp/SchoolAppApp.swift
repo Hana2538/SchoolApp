a0203273fffa7aebb785cfdb61e80b0cff180419
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-12 15:38:22 +0900

    Navigationbarの色追加

--- SchoolApp/SchoolAppApp.swift
+++ SchoolApp/SchoolAppApp.swift
@@ -11,13 +11,11 @@ struct SchoolAppApp: App {
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
         
-        
         appearance.stackedLayoutAppearance.normal.iconColor = .white
-        // iOS 15以降の場合、scrollEdgeAppearanceも設定
+        
         if #available(iOS 15.0, *) {
             UITabBar.appearance().scrollEdgeAppearance = appearance
         }
-
     }
     
     var body: some Scene {
@@ -41,8 +39,39 @@ struct SchoolAppApp: App {
                         .tabItem {
                             Text("Home")
                         }
-                }.accentColor(.orange)
+                }
+                
+                .navigationBarTitle("吾輩はアプリである名前はまだない", displayMode: .inline)
+                .navigationBarColor(backgroundColor: UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1), titleColor: .white) 
             }
         }
+    }
+}
+
+// NavigationBarの色を設定する拡張
+extension View {
+    func navigationBarColor(backgroundColor: UIColor, titleColor: UIColor) -> some View {
+        self.modifier(NavigationBarModifier(backgroundColor: backgroundColor, titleColor: titleColor))
+    }
+}
+
+struct NavigationBarModifier: ViewModifier {
+    var backgroundColor: UIColor
+    var titleColor: UIColor
+    
+    init(backgroundColor: UIColor, titleColor: UIColor) {
+        self.backgroundColor = backgroundColor
+        self.titleColor = titleColor
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = backgroundColor
+        appearance.titleTextAttributes = [.foregroundColor: titleColor]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    func body(content: Content) -> some View {
+        content
     }
 }
