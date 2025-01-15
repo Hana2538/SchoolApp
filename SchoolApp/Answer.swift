992581c914209461e03a5cacf97ef70d75830e87
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-15 21:24:28 +0900

    シートの中でも今までと一緒だね！

---
+++ SchoolApp/Answer.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct Answer: View {
+    var body: some View {
+        NavigationView{
+            Text("Hello, World!")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+        }
+    }
+}
--- SchoolApp/MakeQuestion.swift
+++ SchoolApp/MakeQuestion.swift
@@ -12,7 +12,7 @@ struct TextFieldStyle: ViewModifier {
                 RoundedRectangle(cornerRadius: 8)
                     .stroke(Color.black, lineWidth: 2)
             )
-            .frame(width: 350, height: 50)
+            .frame(width: 340, height: 50)
             .padding(.bottom,20)
     }
 }
@@ -30,58 +30,65 @@ struct MakeQuestion: View {
     @State private var choice2: String = ""
     @State private var choice3: String = ""
     @State private var choice4: String = ""
-
+    
     var body: some View {
-        ZStack {
-            VStack {
-                Text("問題を作成")
-                    .font(.system(size: 30))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.orange)
-                    .multilineTextAlignment(.center)
-
-                Text("問題")
-                    .font(.system(size: 25))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 5)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-
-                TextEditor(text: $inputText)
-                    .padding(10)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 1)
-                    )
-                    .frame(width: 300, height: 150)
-
-                Text("回答")
-                    .font(.system(size: 25))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
-                    .multilineTextAlignment(.center)
-                    .padding(.top, 5)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-
-                TextField("選択肢1", text: $choice1)
-                    .customTextFieldStyle(color: .white)
-
-                TextField("選択肢2", text: $choice2)
-                    .customTextFieldStyle(color: .white)
-
-                TextField("選択肢3", text: $choice3)
-                    .customTextFieldStyle(color: .white)
-
-                TextField("選択肢4", text: $choice4)
-                    .customTextFieldStyle(color: .white)
-
-                Spacer()
+        NavigationView{
+            ZStack {
+                VStack {
+                    Text("問題を作成")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.orange)
+                        .multilineTextAlignment(.center)
+                    
+                    Text("問題")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    TextEditor(text: $inputText)
+                        .padding(10)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        .frame(width: 300, height: 150)
+                    
+                    Text("回答")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 5)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    TextField("選択肢1", text: $choice1)
+                        .customTextFieldStyle(color: .white)
+                    
+                    TextField("選択肢2", text: $choice2)
+                        .customTextFieldStyle(color: .white)
+                    
+                    TextField("選択肢3", text: $choice3)
+                        .customTextFieldStyle(color: .white)
+                    
+                    TextField("選択肢4", text: $choice4)
+                        .customTextFieldStyle(color: .white)
+                    
+                    NavigationLink(destination:Answer()) {
+                        Text("次へ")
+                            .modifier(MyTitle(color: .orange))
+                    }
+                    
+                    Spacer()
+                }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(40)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white)
-        .cornerRadius(10)
-        .padding(40)
     }
 }
