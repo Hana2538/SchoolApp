1dc39e8b87a5f81fb6f463661fe36537abe9a396
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-16 14:08:01 +0900

    Pickerっていうのがあるんだって！ホイールピッカーとかね！

--- SchoolApp/PointView.swift
+++ SchoolApp/PointView.swift
@@ -1,10 +1,47 @@
 import SwiftUI
 
 struct PointView: View {
+    @State private var Point: String = ""
     var body: some View {
         ZStack{
             VStack{
-                // ここに内容を追加することができる
+                Text("問題を作成")
+                    .font(.system(size:30))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.orange)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom,10)
+                
+                Text("この問題のポイントを決めてください")
+                    .font(.system(size:20))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                HStack{
+                    TextField("", text: $Point)
+                        .padding()
+                        .font(.system(size:50))
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                        .padding(.horizontal)
+                        .frame(width: 200, height: 50)
+                    
+                    Text("pt")
+                        .font(.system(size:50))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                    
+                }
+                Spacer()
+                
+                
             }
         }
     }
