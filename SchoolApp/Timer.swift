aedcf6a97d671a34983c509392d3d9a5ddcdd7fd
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-17 23:51:09 +0900

    細かい設定

--- SchoolApp/Timer.swift
+++ SchoolApp/Timer.swift
@@ -18,23 +18,23 @@ struct Timer: View{
                 ZStack {
                             Circle()
                                 .stroke(
-                                    Color.gray,
+                                    Color.orange,
                                     style: StrokeStyle(
-                                        lineWidth: 15, // 線の太さ
+                                        lineWidth: 30, // 線の太さ
                                         lineCap: .round) // 線の端の形
                                 )
 //                                .opacity(0.5)
-                                .frame(width: 300, height: 300)
+                                .frame(width: 250, height: 250)
                             // 上の円
                             Circle()
-                                .trim(from: 0.0, to: 0.5) //ビヨーンてなる線
+                                .trim(from: 0.0, to: 0.5) //どっからどこまでいくかだよん
                                 .stroke(
                                     Color(red: 0, green: 0.4, blue: 0.7),
                                     style: StrokeStyle(
-                                        lineWidth: 15,
+                                        lineWidth: 30,
                                         lineCap: .round)
                                 )
-                                .frame(width: 300, height: 300)
+                                .frame(width: 250, height: 250)
                         }
                 
                 Spacer()
