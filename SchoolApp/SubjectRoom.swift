afd676ad6e2672ba7e8fe0f72304a15265f39e34
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-17 19:23:41 +0900

    名前問題は意外と後でもめんどくさくなさそうだからいいや、SubjectRoomのUI書いた

--- SchoolApp/SubjectRoom.swift
+++ SchoolApp/SubjectRoom.swift
@@ -8,16 +8,35 @@ struct SubjectRoom: View{
                 .scaledToFill()
                 .ignoresSafeArea()
             VStack{
+                
+                Spacer()
+                
                 Text("仮ルーム")
                     .font(.system(size:45))
                     .fontWeight(.bold)
                     .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
                     .multilineTextAlignment(.center)
                 
+                
                 NavigationLink(destination:MakeQuestionBase()){
                     Text("問題を編集")
-                        .modifier(MyTitle(color: .orange, width: 200, height: 50))
+                        .modifier(MyTitle(color: .orange, width: 250, height: 70))
                 }
+                
+                .padding(.bottom,30)
+                
+                NavigationLink(destination:MakeQuestionBase()){
+                    Text("ランキングをチェック")
+                        .modifier(MyTitle(color: .orange, width: 250, height: 70))
+                }
+                
+                .padding(.bottom,30)
+                
+                NavigationLink(destination:MakeQuestionBase()){
+                    Text("タイマー")
+                        .modifier(MyTitle(color: .orange, width: 250, height: 70))
+                }
+                .padding(.bottom,100)
                 
                 Spacer()
             }
