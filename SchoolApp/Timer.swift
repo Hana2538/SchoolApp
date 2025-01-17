5a0a0c2c389f19cc68bdd5e22eab99fc5cb89097
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-17 20:00:22 +0900

    一旦いるものは書いたぞ

--- SchoolApp/Timer.swift
+++ SchoolApp/Timer.swift
@@ -8,19 +8,22 @@ struct Timer: View{
                 .scaledToFill()
                 .ignoresSafeArea()
             
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.white)
-                .frame(width: 350, height: 100)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color.black, lineWidth: 2)
-                )
-            
             VStack{
+                
+                Spacer()
+                
                 Text("Timer")
                     .font(.system(size:50))
                     .fontWeight(.bold)
                     .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.white)
+                    .frame(width: 350, height: 120)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
                 
                 Spacer()
                 
