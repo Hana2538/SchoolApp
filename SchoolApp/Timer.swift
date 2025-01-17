9517410997ccd4a15b3953eede08bb1c0822b155
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-18 00:59:54 +0900

    ピッカー使ったけどなんだかなぁ

--- SchoolApp/Timer.swift
+++ SchoolApp/Timer.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
 struct Timer: View{
+    @State private var Hour = 0
+    @State private var Minutes = 0
+    @State private var Second = 0
     var body: some View{
+        
         ZStack{
             Image("kabegamiNone")
                 .resizable()
@@ -16,30 +20,62 @@ struct Timer: View{
                     .fontWeight(.bold)
                     .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
                 ZStack {
-                            Circle()
-                                .stroke(
-                                    Color.orange,
-                                    style: StrokeStyle(
-                                        lineWidth: 30, // 線の太さ
-                                        lineCap: .round) // 線の端の形
-                                )
-//                                .opacity(0.5)
-                                .frame(width: 250, height: 250)
-                            // 上の円
-                            Circle()
-                                .trim(from: 0.0, to: 0.5) //どっからどこまでいくかだよん
-                                .stroke(
-                                    Color(red: 0, green: 0.4, blue: 0.7),
-                                    style: StrokeStyle(
-                                        lineWidth: 30,
-                                        lineCap: .round)
-                                )
-                                .frame(width: 250, height: 250)
+                    Circle()
+                        .stroke(
+                            Color.orange,
+                            style: StrokeStyle(
+                                lineWidth: 30, // 線の太さ
+                                lineCap: .round) // 線の端の形
+                        )
+                        .frame(width: 280, height: 280)
+                    Circle()
+                        .trim(from: 0.0, to: 0.5) //どっからどこまでいくかだよん
+                        .stroke(
+                            Color(red: 0, green: 0.4, blue: 0.7),
+                            style: StrokeStyle(
+                                lineWidth: 30,
+                                lineCap: .round)
+                        )
+                        .frame(width: 280, height: 280)
+                    
+                    HStack{
+                        Picker(selection: $Second, label: Text("時間選択")) {
+                            ForEach(1 ..< 59) { num in
+                                Text(String(num))
+                                    .font(.system(size: 35))
+                            }
                         }
+                        .pickerStyle(WheelPickerStyle())
+                        .scaleEffect(1.2)
+                        .frame(width: 80, height: 150)
+                        
+                        Picker(selection: $Minutes, label: Text("分選択")) {
+                            ForEach(1 ..< 59) { num in
+                                Text(String(num))
+                                    .font(.system(size: 35))
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        .scaleEffect(1.2)
+                        .frame(width: 80, height: 150)
+
+                        Picker(selection: $Second, label: Text("秒選択")) {
+                            ForEach(1 ..< 59) { num in
+                                Text(String(num))
+                                    .font(.system(size: 35))
+                            }
+                        }
+                        .pickerStyle(WheelPickerStyle())
+                        .scaleEffect(1.2)
+                        .frame(width: 80, height: 150)
+                    }
+                        
+                    }
+                }
                 
                 Spacer()
                 
             }
         }
     }
-}
+
