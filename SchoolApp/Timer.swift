593858bdbf2a4e90b0f9df0de0a6e83df696d89a
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-17 23:32:10 +0900

    タイマーどうしよかなー

--- SchoolApp/Timer.swift
+++ SchoolApp/Timer.swift
@@ -10,20 +10,12 @@ struct Timer: View{
             
             VStack{
                 
-                Spacer()
                 
                 Text("Timer")
                     .font(.system(size:50))
                     .fontWeight(.bold)
                     .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
                 
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color.white)
-                    .frame(width: 350, height: 120)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.black, lineWidth: 2)
-                    )
                 
                 Spacer()
                 
