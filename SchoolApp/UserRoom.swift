911033826c745389439f48d1344e2962d6be4dd6
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-21 16:48:58 +0900

    HStack{}がんばろ

--- SchoolApp/UserRoom.swift
+++ SchoolApp/UserRoom.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct UserRoom: View{
+    @State private var points: Int = 0
     var body:some View{
         ZStack{
             Image("kabegamiNone")
@@ -9,7 +10,49 @@ struct UserRoom: View{
                 .ignoresSafeArea()
             VStack{
                 
+                Text("仮ルーム")
+                    .font(.system(size:50))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
                 
+                Text("問題作成者")
+                    .font(.system(size:20))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                    .padding(.leading,250)
+                
+                Text("ただいまのポイント")
+                    .font(.system(size:20))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+                    .padding(.trailing,200)
+                    .padding(.top,30)
+                
+                HStack {
+                    Spacer() 
+
+                    Text("\(points)")
+                        .font(.title)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray, lineWidth: 2)
+                        )
+                        .frame(width: 100, height: 50)
+
+                    Text("pt")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(red: 0, green: 0.4, blue: 0.7))
+
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+
+                
+                Spacer()
                 
             }
         }
