832e758f81f43d1e872fa2b0b2d68eb68b22e72d
Hana　Yoshimura <huacaijicun7@gmail.com>
2025-01-29 20:49:40 +0900

    Publishedでエラーが消えた。あってるか知らんけど

--- SchoolApp/LogInViewModel 2.swift
+++ SchoolApp/LogInViewModel 2.swift
@@ -1,12 +1,20 @@
+//
+//  LogInViewModel 2.swift
+//  SchoolApp
+//
+//  Created by 吉村花菜 on 2025/01/28.
+//
+
+
 import FirebaseAuth
-import Combine
 
 class LogInViewModel: ObservableObject {
-    @Published var user = User()
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var name: String = ""
     @Published var errorMessage: String? = nil
+    
     func login() {//この関数が呼び出されたら以下のコードのことだよってこと！
-        let email = user.mail
-        let password = user.password
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in//{ authResult, error inはクロージャ関数ってやつで簡単に作りたい時作る
             if let error = error {//もしもエラーが発生したらそれをerrorという名前で取得するってこと
                 DispatchQueue.main.async {//エラーを人間が読める様にするプロパティ
@@ -20,5 +28,24 @@ class LogInViewModel: ObservableObject {
             }
         }
     }
+    
+    func maccount(){
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let user = result?.user {
+                let request = user.createProfileChangeRequest()
+                request.displayName = self.name
+                request.commitChanges { error in
+                    if error == nil {
+                        user.sendEmailVerification() { error in
+                            if error == nil {
+                                print("仮登録画面へ")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
